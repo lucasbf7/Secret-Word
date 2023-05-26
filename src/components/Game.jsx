eddbd481ab9cb251d400f85c2b79e04c8258da3c
lucasbf7 <lucasborges_06@hotmail.com>
2023-05-25 22:37:46 -0300

    First

---
+++ src/components/Game.jsx
@@ -0,0 +1,33 @@
+import './Game.css';
+
+const Game = ({verifyLetter}) => {
+  return (
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name='letter' maxLength={1} required />
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
+    </div>
+  )
+}
+
+export default Game
